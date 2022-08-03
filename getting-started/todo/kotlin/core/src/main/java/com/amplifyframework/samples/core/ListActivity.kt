5efc2cb6844c042bc660425730417aedb89fb97b
aiman-ucrest <aiman@ucrest.net>
2022-08-03 13:22:52 +0800

    renamed files

--- getting-started/todo/kotlin/core/src/main/java/com/amplifyframework/samples/core/ListActivity.kt
+++ getting-started/todo/kotlin/core/src/main/java/com/amplifyframework/samples/core/ListActivity.kt
@@ -2,14 +2,14 @@ package com.amplifyframework.samples.core
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.amplifyframework.samples.core.databinding.ActivityMainBinding
+import com.amplifyframework.samples.core.databinding.ActivityListBinding
 
 abstract class ListActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityListBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
