43a8b07fade65db1c9a9070ecc1ee106a97d6308
aiman-ucrest <aiman@ucrest.net>
2022-08-03 13:02:51 +0800

    Initialize Amplify Auth

--- getting-started/todo/kotlin/core/src/main/java/com/amplifyframework/samples/core/MainApplication.kt
+++ getting-started/todo/kotlin/core/src/main/java/com/amplifyframework/samples/core/MainApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.util.Log
 import com.amplifyframework.AmplifyException
 import com.amplifyframework.api.aws.AWSApiPlugin
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
 import com.amplifyframework.core.Amplify
 import com.amplifyframework.datastore.AWSDataStorePlugin
 
@@ -14,6 +15,8 @@ open class MainApplication : Application() {
         try {
             Amplify.addPlugin(AWSDataStorePlugin())
             Amplify.addPlugin(AWSApiPlugin())
+            // Add this line, to include the Auth plugin.
+            Amplify.addPlugin(AWSCognitoAuthPlugin())
             Amplify.configure(applicationContext)
 
             Log.i("MyAmplifyApp", "Initialized Amplify")
