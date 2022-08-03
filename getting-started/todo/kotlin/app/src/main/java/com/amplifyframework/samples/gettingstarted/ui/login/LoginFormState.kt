deae1198a15602e23a5db402a9f5284f84d3fc7e
aiman-ucrest <aiman@ucrest.net>
2022-08-03 13:10:17 +0800

    added LoginActivity.kt & related resources
    auto-generated

---
+++ getting-started/todo/kotlin/app/src/main/java/com/amplifyframework/samples/gettingstarted/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package com.amplifyframework.samples.gettingstarted.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
