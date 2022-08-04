501a910098a121f505c3a8f76c0ba9366d940b58
aiman-ucrest <aiman@ucrest.net>
2022-08-04 13:45:45 +0800

    server reply every ping

--- websocket/server.js
+++ websocket/server.js
@@ -7,6 +7,7 @@ const wss = new WebSocket.Server({ port: 8080 })
 wss.on('connection', ws => {
   ws.on('message', message => {
     console.log(`Received message => ${message}`)
+    ws.send('Pong!')
   })
   ws.send('Hello! Message From Server! :)')
 })
