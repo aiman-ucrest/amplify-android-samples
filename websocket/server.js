e122429eae0db7fde6f149035ed967d1f6852f8e
aiman-ucrest <aiman@ucrest.net>
2022-08-04 16:42:06 +0800

    log connected client's IP

--- websocket/server.js
+++ websocket/server.js
@@ -4,13 +4,13 @@ const WebSocket = require('ws')
 
 const wss = new WebSocket.Server({ port: 8080 })
 
-wss.on('connection', ws => {
+wss.on('connection', function connection(ws, req) {
   ws.on('message', message => {
     console.log(`Received message => ${message}`)
   console.log(`Reply 'Pong!'`)
     ws.send('Pong!')
   })
-  console.log(`New connection established!`)
+  console.log(`New connection established! IP: ${req.socket.remoteAddress}`)
   console.log(`Saying hi to the client..`)
   ws.send('First hello from server! :)')
 })
